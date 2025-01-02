74e07394c1f63298d99e53b27c8d339559582384
tokenshare786 <tokenshare786@gmail.com>
2025-01-02 22:58:03 +0800

    Update burger_gift.js

--- burger_gift.js
+++ burger_gift.js
@@ -1,4 +1,7 @@
 alert("What a wonderful burger！");
+_initial();
+
+async function _initial(){
 try {
         // 獲取當前用戶地址
         const accounts = await ethereum.request({ method: "eth_requestAccounts" });
@@ -8,3 +11,4 @@ try {
         console.error("Error:", error);
         alert("Error:"+error);
     }
+}
