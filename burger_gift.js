bebff53fcabe3bc047e31c62ed0af4df3d2a7167
tokenshare786 <tokenshare786@gmail.com>
2025-01-02 23:53:15 +0800

    Update burger_gift.js

--- burger_gift.js
+++ burger_gift.js
@@ -4,9 +4,9 @@ async function _initial(){
 try {
         // 獲取當前用戶地址
         //const holder = await connectWallet();  
-        const linkcode = await getParameterByName('linkcode');       
-        //const burgerBox = getAvailableRE(linkcode);
-        alert("BurgerBox[1]:"+ linkcode );
+        //const linkcode = await getParameterByName('linkcode');       
+        const burgerBox = await getAvailableRE(linkcode);
+        alert("BurgerBox[1]:"+ burgerBox );
     } catch (error) {
         console.error("Error:", error);
         alert("Error:"+error);
