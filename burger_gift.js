068c67bb5c8febbccf90de31df1096f6a39775d7
tokenshare786 <tokenshare786@gmail.com>
2025-01-02 23:05:06 +0800

    Update burger_gift.js

--- burger_gift.js
+++ burger_gift.js
@@ -6,6 +6,10 @@ try {
         // 獲取當前用戶地址
         const holder = await connectWallet();                
         alert("data processing:"+ holder );
+        // 調用智能合約的 getSpecificRE 函數
+        const availableRE = await contract.methods
+            .getSpecificRE(1)
+            .call();
     } catch (error) {
         console.error("Error:", error);
         alert("Error:"+error);
