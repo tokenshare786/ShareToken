21808a96fd6c8fa3fc65c37d5607e155491c1426
tokenshare786 <tokenshare786@gmail.com>
2025-01-02 22:46:43 +0800

    Update burger_gift.js

--- burger_gift.js
+++ burger_gift.js
@@ -1,10 +1,10 @@
 alert("What a wonderful burger！")
 try {
         // 獲取當前用戶地址
-        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-        const userAddress = accounts[0];        
+        //const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        //const userAddress = accounts[0];        
         alert("data processing"+ userAddress );
     } catch (error) {
         console.error("Error fetching available red envelopes:", error);
-        alert("An error occurred while fetching available red envelopes.");
+        alert("Error:"+error);
     }
