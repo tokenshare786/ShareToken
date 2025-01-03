4a028cfb3ae12eb35e5244c2d650b1217fb7933f
tokenshare786 <tokenshare786@gmail.com>
2025-01-03 15:27:19 +0800

    Update burger_gift.js

--- burger_gift.js
+++ burger_gift.js
@@ -34,7 +34,7 @@ async function loadburgerBoxPage(item) {
                  <h2>${web3.utils.hexToAscii(item.desc)}</h2>
                      <p class="reward-item">${item.claimedAmt} / ${item.subAmt}</p>
                      <p class="reward-item">${item.eligiType} : ${item.claimCount} /  ${item.maxClaims}</p>
-                     <p class="reward-item">${formattedTime}</p>
+                     <p class="reward-item">${startTime}</p>
             </div>
             <div class="image-container">
                <img src="${item.imgUrl}" alt="photo">  
