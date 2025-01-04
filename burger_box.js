3e670f43df2452b711c65914d0b27581836de859
tokenshare786 <tokenshare786@gmail.com>
2025-01-04 21:33:19 +0800

    Update burger_box.js

--- burger_box.js
+++ burger_box.js
@@ -1,1 +1,25 @@
 alert("Haha");
+initial();  
+
+let re;
+let re_id;
+let burger_count;
+
+function initial(){
+        alert("Hi..What's up");
+}
+
+async function _initial(){
+        alert("something...");
+try {       
+        //const reid = prompt("Enter the RedEnvelope ID:");
+        burger_count = await getreID();
+        re_id = burger_count;
+        alert("re_id:"+_re_id);
+        re = await getSpecificRE(burger_count);     
+        //await loadburgerBoxPage(re);  
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Error:"+error);
+    }
+}
