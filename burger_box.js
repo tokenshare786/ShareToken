8f2652754a2369673466c8dfcf6e8a8a740bfbb4
tokenshare786 <tokenshare786@gmail.com>
2025-01-04 21:54:33 +0800

    Update burger_box.js

--- burger_box.js
+++ burger_box.js
@@ -10,14 +10,81 @@ async function initial(){
 try {       
         //const reid = prompt("Enter the RedEnvelope ID:");
         burger_count = await getreID();
-        alert("burger_count:"+burger_count);
+        //alert("burger_count:"+burger_count);
         //
         re_id = burger_count;
-        alert("re_id:"+ re_id);
+        //alert("re_id:"+ re_id);
         re = await getSpecificRE(burger_count);     
-        //await loadburgerBoxPage(re);  
+        await loadburgerBoxPage(re);  
     } catch (error) {
         console.error("Error:", error);
         alert("Error:"+error);
     }
+}
+
+let card;
+
+// Load burgerBoxPage and Display a Single Result
+async function loadburgerBoxPage(item) {
+    try {    
+        if (!item) {
+            alert("No red envelope found.");
+            //resultTable.hidden = true;
+            return;
+        }
+         card  = document.getElementById("card");
+         //const content = document.getElementById("content");
+         //card_html = card.innerHTML ;
+         card.innerHTML = "";
+         //card_html.style.display= "invisible";           
+         const row = document.createElement("div");
+         row.id = "burgerbox";        
+         //row.classList.add("progress");
+         const startTime = new Date(Number(item.startTime) * 1000).toLocaleString();        
+         row.innerHTML = `
+            <div>
+                     <h2>${web3.utils.hexToAscii(item.desc)}</h2>
+                     <p class="reward-item">【${item.eligiType}】 ${startTime} & ${item.claimedAmt} / ${item.subAmt} : ${item.claimCount} / ${item.maxClaims}</p>
+                     <span class="progress">
+                         <p class="css_back" onclick="_back()">Back</p>
+                         <p class="css_back" style="margin-left:auto" onclick="_next()">Next</p>
+                     <span>
+            </div>
+            <div  class="new-container">
+               <div class="image-container" onclick="claim_re()">
+                  <img src="${item.imgUrl}" alt="photo">  
+               </div>            
+            </div>
+            `;
+           card.innerHTML = row.innerHTML ; 
+           //content.appendChild(row);           
+           
+    } catch (err) {
+        console.error("Error loading content:", err);
+        alert("Failed to display Burgerbox." + err);            
+    }
+}
+function claim_re(){   
+    alert("正要領取漢堡盒：" + re_id);
+    claimRE(re_id);
+}
+
+function _back(){
+        if(re_id > 1){
+              re_id --;
+              re = await getSpecificRE(re_id);     
+              await loadburgerBoxPage(re);    
+        }else{
+              alert("前面沒有漢堡盒了！");
+        }        
+}
+
+function _next(){
+        if(re_id < burger_count){
+              re_id ++ ;
+              re = await getSpecificRE(re_id);     
+              await loadburgerBoxPage(re);    
+        }else{
+              alert("後面沒有漢堡盒了！");
+        }        
 }
