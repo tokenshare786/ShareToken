ef5fbc2bc411c723651cd9710439e924bb2baa9d
tokenshare786 <tokenshare786@gmail.com>
2025-01-07 19:05:50 +0800

    Update dona_box.js

--- dona_box.js
+++ dona_box.js
@@ -1,2 +1,130 @@
 alert("Updated! 10");
 
+var re_id = await getreID();
+var re = await getSpecificRE(re_id);
+const userAddress = await getHoldertoLowercase();
+return;
+try {   
+        if (!re) {
+            alert("目前沒有甜甜圈 ><");
+            return;
+        }
+        await loadburgerBoxPage();  
+    } catch (error) {
+        alert("initial Error:"+error);
+    }
+
+let card;
+let eligible;
+let isactive;
+
+// Load burgerBoxPage and Display a Single Result
+async function loadburgerBoxPage() {
+        isactive = re.isActive ;
+        if(isactive){
+             eligible  = await checkEgibility(re_id);
+        }        
+        //alert("eligible:" + eligible);
+    try {  
+         card  = document.getElementById("card");
+         //const content = document.getElementById("content");
+         card.innerHTML = "";
+         const row = document.createElement("div");
+         row.id = "burgerbox";        
+         //row.classList.add("progress");
+         const startTime = new Date(Number(re.startTime) * 1000).toLocaleString();        
+         row.innerHTML = `
+            <div>
+                     <h2>${re.desc}</h2>
+                     <p class="reward-item">【${re.eligiType}】 ${startTime} & ${re.claimedAmt} / ${re.subAmt} : ${re.claimCount} / ${re.maxClaims}</p>
+                     <span class="progress">
+                         <p class="css_back" onclick="_back()">Back</p>                         
+                         <p class="css_back" style="margin-left:auto" onclick="_next()">Next</p>
+                     </span>
+            </div>
+            <div  class="new-container">
+               <div class="image-container" onclick="claim_re()">
+                  <img src="${re.imgUrl}" alt="photo">  
+               </div>            
+            </div>
+            <span class="progress">
+                         <p class="css_back" onclick="open_edit()" id="editable">Edit</p>
+            </span>
+            `;   
+            //alert("here..");            
+           card.innerHTML = row.innerHTML ; 
+           //content.appendChild(row);              
+            //const holder = await getHoldertoLowercase();
+            const re_creator = re.creator.toLowerCase() ;
+            //alert("item.creator:\n"+item.creator+"\ngetHoldertoLowercase():\n"+holder);
+            if( re_creator !== userAddress){
+                  //alert("any problem?");  
+                  document.getElementById("editable").style.display = "none";
+                  alert("you're not creator.");  
+            } else {
+                  alert("It's editable！");  
+                  document.getElementById("editable").style.display = "block";
+            }          
+    } catch (err) {
+        //console.error("Error loading content:", err);
+        alert("Failed to display:" + err);            
+    }
+}
+
+function claim_re(){ 
+        if(isactive){
+              if(eligible){
+                   if(confirm("準備吃個甜甜圈：" + re_id)){            
+                            claimRE(re_id); 
+                   } else{
+                           alert("Why not?"); 
+                   }  
+             } else {
+                         alert("這個甜甜圈你吃不了：" + re_id);
+             }   
+        } else {
+                  alert("這個甜甜圈沒了：" + re_id);
+        }          
+}
+
+async function _next() {
+    try {
+        if (re_id < burger_count) {
+            re_id++; // 增加 re_id
+            re = await getSpecificRE(re_id); // 獲取下一個漢堡盒的數據
+
+            if (re) {
+                await loadburgerBoxPage(); // 加載頁面
+            } else {
+                alert("未找到對應的甜甜圈！");
+                re_id--; // 如果數據不存在，還原 re_id
+            }
+        } else {
+            alert("後面沒有甜甜圈了！"); // 提示訊息
+        }
+    } catch (error) {
+        //console.error("烘焙甜甜圈時出現錯誤：", error);
+        alert("烘焙甜甜圈時出現怪事，稍等！");
+    }
+}
+
+async function _back() {
+    try {
+        if (re_id > 1) {
+            re_id--; // 減少 re_id
+            re = await getSpecificRE(re_id); // 獲取指定漢堡盒的數據
+            if (re) {
+                await loadburgerBoxPage(re); // 加載頁面
+            } else {
+                alert("未找到對應的甜甜圈數據！");
+                re_id++; // 如果數據不存在，還原 re_id
+            }
+        } else {
+            alert("前面沒有甜甜圈了！"); // 提示
+        }
+    } catch (error) {
+        console.error("切換甜甜圈時出現錯誤：", error);
+        alert("發生錯誤，請稍後再試！");
+    }
+}
+
