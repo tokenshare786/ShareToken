9451a94b0375064e39fefaa90b5e53123252ece2
tokenshare786 <tokenshare786@gmail.com>
2025-01-07 19:59:58 +0800

    Update dona_box.js

--- dona_box.js
+++ dona_box.js
@@ -1,10 +1,11 @@
-alert("Updated! 15");
+alert("Updated! 16");
+
 let re_id;
 let re;
-//let card;
-//let eligible;
-//let isactive;
-//let userAddress;
+let card;
+let eligible;
+let isactive;
+let userAddress;
 
 
 
