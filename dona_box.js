1ae5883edd9fc28e0053c79449da4acd52264781
tokenshare786 <tokenshare786@gmail.com>
2025-01-07 20:05:24 +0800

    Update dona_box.js

--- dona_box.js
+++ dona_box.js
@@ -7,5 +7,23 @@ let eligible;
 let isactive;
 let userAddress;
 
+async function initializePage() {
+try {   
+        re_id = await getreID();
+        if(re_id > 0){
+             re = await getSpecificRE(re_id);
+        } else {
+                alert("目前沒有甜甜圈 ><");
+                return;
+        }  
+        await loadburgerBoxPage();  
+    } catch (error) {
+        alert("initial Error:" + error);
+    }
+  
+}
+alert("here..");
+initializePage();
+
 
 
