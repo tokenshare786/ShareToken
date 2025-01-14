01c4ee9ac98757e39fec6a0d954ac1f307133097
tokenshare786 <tokenshare786@gmail.com>
2025-01-14 20:41:44 +0800

    Update dona_streaming.js

--- dona_streaming.js
+++ dona_streaming.js
@@ -13,8 +13,6 @@ async function initializePage() {
         //alert('donaCount:' + donaCount);
         if (donaCount > 0) {
             await loadMoreDonaBoxes(); // 加载第一批 Dona_box
-            //user_ds = await getHoldertoLowercase();
-            //alert('user_ds:'+user_ds);
         } else {
             alert("目前没有甜甜圈 ><");
         }
@@ -86,9 +84,9 @@ async function createDonaBox(re, re_id) {
             </div>            
         </div>
         <span class="progress">
-            <p class="css_back" onclick="open_edit()" id="editable-ds">Edit</p>
-            <p class="css_back" onclick="shareDona(${re_id})">Share</p>
+            <p class="css_back" onclick="open_edit(${re_id})" id="editable-ds">Edit</p>
             <p class="css_back" onclick="claimDN(${re_id})" id="take-ds">Take</p> 
+            <p class="css_back" onclick="shareDona(${re_id})">Share</p>            
         </span>
     `;
     container.appendChild(card);
@@ -171,4 +169,53 @@ function debounce(func, wait) {
 
 // 初始化页面
 initializePage();
+let dnid_edit;
+function open_edit(dnid) {   
+      dnid_edit = dnid;
+      const dona = await getSpecificDN(dnid); // 获取数据
+      const overlay = document.getElementById('edit_window');
+      overlay.classList.add('show');
+      //alert("re:"+ re.desc + "/" +  re.imgUrl);
+      document.getElementById("edit_desc").value = dona.desc;             
+      document.getElementById("edit_url").value = dona.imgUrl;
+}
+
+// Close modal
+function close_edit() {
+      //document.getElementById("edit_window").style.display = "none";
+      const overlay = document.getElementById('edit_window');
+      overlay.classList.remove('show'); 
+}           
+             
+document.getElementById("form_edit").addEventListener("submit", async (event) => {
+        event.preventDefault(); // 防止表單默認提交行為
+              //alert("What's up?");
+              await editDona(); // 確保執行智能合約的邏輯
+});
+
+async function editDona(){
+        // Get form data
+        const dona = await getSpecificDN(dnid_edit);
+        const edit_desc = document.getElementById("edit_desc").value;
+        const edit_url = document.getElementById("edit_url").value;       
+        //alert("editDona:"+ edit_desc  + "/" +  edit_url);
+      try{
+        // 呼叫智能合约的 setRE 函式
+        if( edit_desc != dona.desc || edit_url != dona.imgUrl){                
+        await contract.methods
+            .updateMyDona(
+                dnid_edit,                   
+                edit_desc,
+                edit_url
+            )
+            .send({ from: _useraddress });
+        }             
+
+        // 關閉彈跳視窗
+        close_edit();
+        loadMoreDonaBoxes();
+        } catch (error) {
+            console.error("Error:", error);
+        }     
+    }
 
