b404d9df462ac5a499af1a963ce29303a799c2da
tokenshare786 <tokenshare786@gmail.com>
2025-01-14 15:21:19 +0800

    Update dona_streaming.js

--- dona_streaming.js
+++ dona_streaming.js
@@ -1,4 +1,4 @@
-alert('Im new 11');
+alert('Im new 12');
 let currentPage = 1; // 当前页数
 const itemsPerPage = 5; // 每次加载数量
 let isLoading = false; // 是否正在加载中
@@ -36,10 +36,10 @@ async function loadMoreDonaBoxes() {
         try {
             const dona = await getSpecificDN(i); // 获取数据
             if (dona) {
-               //await createDonaBox(dona, i); 
-               alert('got dona');
+               await createDonaBox(dona, i); 
+               //alert('got dona');
             } else {
-                alert('no dona now!');
+               //alert('no dona now!');
             }           
         } catch (error) {
             console.error(`Error loading Dona_box ${i}:`, error);
@@ -51,8 +51,80 @@ async function loadMoreDonaBoxes() {
     isLoading = false;
     document.getElementById('loading').style.display = 'none';
 }
+//
+// 创建 Dona_box 的 DOM 元素
+async function createDonaBox(re, re_id) {
+    const container = document.getElementById('dona-container');
+    const card = document.createElement('div');
+    card.className = 'dona-streaming'; // 添加样式类
 
+    const startTime = new Date(Number(re.startTime) * 1000).toLocaleString();
+    const desc = re.desc; // 假設這是描述文本
+    const maxLength = 13; // 最大顯示字數
+    let toolong = false;
+    if (desc.length > maxLength) {
+             toolong = true;
+    } 
+    const shortDesc = desc.slice(0, maxLength);
+    const remainingDesc = desc.slice(maxLength);
+    card.innerHTML = `
+        <div>
+            <div id="desc-container-ds" class="desc-container">
+                <span class="short-desc">${shortDesc}</span>
+                <span id="ellipsis-ds">&nbsp..</span>
+                <span id="toggle-link-ds" class="css_back" style="position:relative;top:5px;left:3px">全文</span>   
+            </div>   
+            <p id="remaining-desc-ds" class="remaining-desc">${remainingDesc}</p>
+            <p class="reward-item" style="text-align:center">
+                【${re.eligiType}】 ${startTime} & ${re.claimedAmt} / ${re.subAmt} : ${re.claimCount} / ${re.maxClaims}
+            </p>            
+        </div>
+        <div class="new-container" style="position:relative;top:7px">
+            <div class="image-container" onclick="claimDN(${re_id})"> 
+                <img src="${re.imgUrl}" alt="photo">  
+            </div>            
+        </div>
+        <span class="progress">
+            <p class="css_back" onclick="open_edit()" id="editable-ds">Edit</p>
+            <p class="css_back" style="margin-left:auto" onclick="claimDN(${re_id})" id="take-ds">Take</p> 
+        </span>
+    `;
+    container.appendChild(card);
 
+    // 显示完整描述逻辑
+    // 初始化縮略文本    
+    //document.getElementById("short-desc").textContent = shortDesc;  
+    // 顯示完整描述邏輯
+    const toggleLink = card.querySelector('#toggle-link-ds');
+    const remainingDescElement = card.querySelector('#remaining-desc-ds');
+    const ellipsis = card.querySelector('#ellipsis-ds');
+    toggleLink.addEventListener('click', () => {
+        remainingDescElement.style.display = 'inline';
+        ellipsis.style.display = 'none';
+        toggleLink.style.display = 'none';
+    });
+    if(!toolong){        
+       //remainingDescElement.style.display = "none";
+       ellipsis.style.display = "none";
+       toggleLink.style.display = "none";
+    }   
+ 
+    const takeElement = card.querySelector('#take-ds');
+    const eligible = await checkEgibility(re_id);
+    if (re.isActive && eligible) {           
+             takeElement.style.display = "block";                
+    } else {
+             takeElement.style.display = "none"; 
+    }         
+    const re_creator = re.creator.toLowerCase() ;
+    const editElement = card.querySelector('#editable-ds');
+    if( re_creator !== userAddress){ 
+              editElement.style.display = "none"; 
+    } else {
+              editElement.style.display = "block";
+    }     
+}
+//
 // 滚动事件监听，实现无限滚动
 window.addEventListener('scroll', debounce(() => {
     if (
