37aa3e8b641a1f8d9d14df53d7efdd09c1e52d17
tokenshare786 <tokenshare786@gmail.com>
2025-01-14 14:11:04 +0800

    Update dona_streaming.js

--- dona_streaming.js
+++ dona_streaming.js
@@ -1,1 +1,21 @@
-alert('Im new');
+alert('Im new 2');
+let currentPage = 1; // 当前页数
+const itemsPerPage = 5; // 每次加载数量
+let isLoading = false; // 是否正在加载中
+let donaCount = 0; // 总甜甜圈数量
+let user_ds;
+// 初始化页面
+async function initializePage() {
+    try {
+        donaCount = await getreID(); // 获取甜甜圈总数
+        donaCount = Number(donaCount); // 确保是数字
+        if (burgerCount > 0) {
+            //await loadMoreDonaBoxes(); // 加载第一批 Dona_box
+            userAddress = await getHoldertoLowercase();
+        } else {
+            alert("目前没有甜甜圈 ><");
+        }
+    } catch (error) {
+        alert("initial Error: " + error);
+    }
+}
