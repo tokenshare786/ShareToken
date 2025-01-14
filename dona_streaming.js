3a12267ca5b2329064ba9b536a33d1043cfe2c4b
tokenshare786 <tokenshare786@gmail.com>
2025-01-14 14:19:57 +0800

    Update dona_streaming.js

--- dona_streaming.js
+++ dona_streaming.js
@@ -1,4 +1,4 @@
-alert('Im new 2');
+alert('Im new 3');
 let currentPage = 1; // 当前页数
 const itemsPerPage = 5; // 每次加载数量
 let isLoading = false; // 是否正在加载中
@@ -10,7 +10,7 @@ async function initializePage() {
         donaCount = await getreID(); // 获取甜甜圈总数
         donaCount = Number(donaCount); // 确保是数字
         if (burgerCount > 0) {
-            //await loadMoreDonaBoxes(); // 加载第一批 Dona_box
+            await loadMoreDonaBoxes(); // 加载第一批 Dona_box
             userAddress = await getHoldertoLowercase();
         } else {
             alert("目前没有甜甜圈 ><");
@@ -18,4 +18,29 @@ async function initializePage() {
     } catch (error) {
         alert("initial Error: " + error);
     }
+}
+async function loadMoreDonaBoxes() {
+    if (isLoading || currentPage > Math.ceil(burgerCount / itemsPerPage)) return;
+
+    isLoading = true;
+    document.getElementById('loading').style.display = 'block';
+
+    const start = (currentPage - 1) * itemsPerPage + 1; // 起始 re_id
+    const end = Math.min(currentPage * itemsPerPage, burgerCount); // 结束 re_id
+
+    for (let i = end ; i >= start ; i--) {
+        try {
+            const dona = await getSpecificDN(i); // 获取数据
+            if (dona) {
+               //await createDonaBox(dona, i); 
+               alert('got dona');
+            }
+        } catch (error) {
+            console.error(`Error loading Dona_box ${i}:`, error);
+        }
+    }
+
+    currentPage++;
+    isLoading = false;
+    document.getElementById('loading').style.display = 'none';
 }
