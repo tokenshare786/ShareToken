7072f8baa9557ba1fef17f26b836ed2074a0ebab
tokenshare786 <tokenshare786@gmail.com>
2025-01-16 20:20:10 +0800

    Update after.js

--- after.js
+++ after.js
@@ -1,8 +1,8 @@
 import app from './firebaseConfig'; // 引入 Firebase 配置
 
-import { getDatabase, ref, get, update, remove, push, set, child } from "firebase/database";
+//import { getDatabase, ref, get, update, remove, push, set, child } from "firebase/database";
 
 // 獲取 Realtime Database 實例
-const database = getDatabase(app);
+//const database = getDatabase(app);
 
 alert('after all 2');
