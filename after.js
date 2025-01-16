56303f2672a82ac3ad20267d4070875c4b8e7ddb
tokenshare786 <tokenshare786@gmail.com>
2025-01-16 23:48:15 +0800

    Update after.js

--- after.js
+++ after.js
@@ -54,4 +54,4 @@ document.getElementById("comment-form").addEventListener("submit", async (event)
     await addComment(_dnid, _commentid, _useraddress, _message); // 確保執行智能合約的邏輯
 });
 
-alert('after all 16');
+alert('after all 17');
