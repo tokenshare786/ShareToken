a8979b77bdc8232afe846c254627f31bad985019
tokenshare786 <tokenshare786@gmail.com>
2025-01-16 21:01:38 +0800

    Update after.js

--- after.js
+++ after.js
@@ -1,8 +1,24 @@
-import app from './firebaseConfig'; // 引入 Firebase 配置
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-//import { getDatabase, ref, get, update, remove, push, set, child } from "firebase/database";
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBBXYWCp5OfBakqTqJ9YaEFUjoWwRxsFp8",
+    authDomain: "blkdona.firebaseapp.com",
+    databaseURL: "https://blkdona-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "blkdona",
+    storageBucket: "blkdona.firebasestorage.app",
+    messagingSenderId: "975346537680",
+    appId: "1:975346537680:web:acef40c00988add3ee3ad5",
+    measurementId: "G-RE8D65CLWZ"
+  };
 
-// 獲取 Realtime Database 實例
-//const database = getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
-alert('after all 5');
+alert('after all 6');
