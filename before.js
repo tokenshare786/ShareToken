ac0213d84c960c54e37135ef7bb9730f694eb67d
tokenshare786 <tokenshare786@gmail.com>
2025-01-17 12:14:50 +0800

    Update before.js

--- before.js
+++ before.js
@@ -1,4 +1,6 @@
 let _useraddress;
+ window._useraddress = _useraddress;
+
       function copyAddress() {
           // 获取合约地址文本
           const contractAddress = "0xCdB05222c8a059a6c0e9202d62fDFb05D6B5D274";
