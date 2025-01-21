c5ea920d68f7a2b45e1e3f49bf5f6a9b7c7c28fa
tokenshare786 <tokenshare786@gmail.com>
2025-01-21 21:49:45 +0800

    Update after.js

--- after.js
+++ after.js
@@ -6,26 +6,26 @@ import { getDatabase, ref, get, update, remove, push, set, child } from "https:/
 // TODO: Add SDKs for Firebase products that you want to use
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//const firebaseConfig = {
-//    apiKey: "AIzaSyBBXYWCp5OfBakqTqJ9YaEFUjoWwRxsFp8",
-//    authDomain: "blkdona.firebaseapp.com",
-//    databaseURL: "https://blkdona-default-rtdb.asia-southeast1.firebasedatabase.app",
-//    projectId: "blkdona",
-//    storageBucket: "blkdona.firebasestorage.app",
-//    messagingSenderId: "975346537680",
-//    appId: "1:975346537680:web:acef40c00988add3ee3ad5",
-//    measurementId: "G-RE8D65CLWZ"
-//};
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyBBXYWCp5OfBakqTqJ9YaEFUjoWwRxsFp8",
+    authDomain: "blkdona.firebaseapp.com",
+    databaseURL: "https://blkdona-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "blkdona",
+    storageBucket: "blkdona.firebasestorage.app",
+    messagingSenderId: "975346537680",
+    appId: "1:975346537680:web:acef40c00988add3ee3ad5",
+    measurementId: "G-RE8D65CLWZ"
 };
+//const firebaseConfig = {
+//    apiKey: process.env.REACT_APP_API_KEY,
+//    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//    databaseURL: process.env.REACT_APP_DATABASE_URL,
+//    projectId: process.env.REACT_APP_PROJECT_ID,
+//    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//    appId: process.env.REACT_APP_APP_ID,
+//    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+//};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
